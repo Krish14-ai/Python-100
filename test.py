5b27bb926f30b3db0434dd0e0fd9b52f458381c6
krish14-ai <kkhandelwal292@gmail.com>
2026-08-21 00:00:45 +0530

    tryi

--- test.py
+++
@@ -1,5 +0,0 @@
-vowels = ['a','e','i','o','u'] 
-str = input("Enter a string : ")
-count = 0   
-count = sum(1 for char in str.lower() if char in vowels)
-print("Number of vowels in the string is : ", count)
